b0cf2c015f0d12667f369d0204a0bc56a2ac6ca5
Sourab <shubpramanik241@gmail.com>
2022-01-28 10:43:27 +0530

    scroll progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,22 @@ import Header from "./components/header";
 import CustomCursor from "./components//customCursor";
 import GlobalProvider from "./context/globalContext";
 import NavPage from "./components/navPage";
+import gsap from "gsap";
+import { useEffect, useRef } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 function MyApp({ Component, pageProps }) {
+  const progressTrack = useRef();
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.to(progressTrack.current, {
+    value: 100,
+    ease: "none",
+    scrollTrigger: { scrub: 0.3 },
+  });
   return (
     <div className="app">
       <GlobalProvider>
+        <progress ref={progressTrack} max="100" value="0"></progress>
         <CustomCursor />
         <NavPage />
         <Header />
