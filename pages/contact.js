243474776f7efd4bf7e570028b082d5637b3fb61
sourabpramanik <shubpramanik241@gmail.com>
2022-10-18 08:30:10 +0530

    cursor transitions

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
 const ContactPage = () => {
   return (
@@ -7,17 +7,17 @@ const ContactPage = () => {
       <div className="page__container">
         <div className="contact_setion-container">
           <div className="contact_l-section">
-            <h1 className="t-heading-600 contact_page">Let's talk</h1>
+            <h1 className="t-heading-600 contact_page">Lets talk</h1>
             <div className="social-container">
               <Link href="/">
                 <a>svg</a>
-              </Link>            
+              </Link>
             </div>
           </div>
           <div className="contact_r-section"></div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 export default ContactPage;
